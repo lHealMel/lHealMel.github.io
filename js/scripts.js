f70d895139605fe9e3615619056c889e4d303aff
lHealMel <50746821+lHealMel@users.noreply.github.com>
2021-03-30 22:47:41 +0900

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,32 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        if(this.scrollY > 20)
-        $(".navbar").addClass("sticky");
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if (this.scrollY > 20)
+            $(".navbar").addClass("sticky");
         else
-        $(".navbar").removeClass("sticky");
+            $(".navbar").removeClass("sticky");
     });
 
-    $('.menu-toggler').click(function(){
+    $('.menu-toggler').click(function () {
         $(this).toggleClass("active");
         $(".navbar-menu").toggleClass("active");
     });
 
+    $(".skill-per").each(function () {
+        var $this = $(this);
+        var per = $this.attr('per');
+        $this.css("width", per + '%');
+        $({ animatedValue: 0 }).animate({ animatedValue: per }, {
+            duration: 1000,
+            step: function () {
+                $this.attr('per', Math.floor(this.animatedValue) + '%');
+            },
+            complete: function () {
+                $this.attr('per', Math.floor(this.animatedValue) + '%');
+            }
+        });
+    });
+
+    $(window).on("load", function () {
+        $(".loader-container").fadeOut(1000);
+    });
 });
